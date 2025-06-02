67343f14c8afb75481c92adfbf617c8ebe67ea5f
Igor <igorcat100@gmail.com>
2025-06-02 22:00:38 +0300

    feature/HEALTHCARECW-15: added redis status sync

--- db/db_session.py
+++ db/db_session.py
@@ -1,9 +1,20 @@
 from db.engine import db_engine
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+)
+from typing import AsyncGenerator
+from contextlib import asynccontextmanager
 
 
-async def get_session():
-    session = db_engine.create_session()
-    try:
+@asynccontextmanager
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Асинхронный контекстный менеджер, возвращающий AsyncSession.
+    Используйте в хэндлерах как:
+        async with get_session() as session:
+            ...
+    Сессия автоматически закроется при выходе из блока.
+    """
+    async with db_engine.create_session() as session:
         yield session
-    finally:
-        session.close()
